6fdc9ee7f121220b96e0ff36f289a75b371eb83e
Nagendra Mahesh <namahesh@cisco.com>
2016-12-28 12:11:47 +0530

    Modularized XML parsing, formatted transcript extraction

--- bulk-transcript-downloader.py
+++ bulk-transcript-downloader.py
@@ -26,10 +26,25 @@ import time
 import xml.etree.ElementTree as ElementTree
 
 # CONSTANTS
-SEARCH_API_URL = "http://{}/ccp-webapp/ccp/search/contacts?q=sc.sourceType:chat%20AND%20sc.socialContactStatus:handled"
+SEARCH_API_URL      = "http://{}/ccp-webapp/ccp/search/contacts?q=sc.sourceType:chat%20AND%20sc.socialContactStatus:handled"
+TIMESTAMP_FORMAT    = "%Y-%m-%d %H:%M:%S %Z"
+TIME_FORMAT         = "%H:%M:%S"    # for every chat message, just need the time of day
+
+TRANSCRIPT_METADATA     = """+--------------- WEB CHAT TRANSCRIPT ---------------+
+| Exported from Cisco SocialMiner [{0}] by '{1}' at {2}
+|
+| ID:           {3}
+| Customer:     {4}
+| Started:      {5}
+| Ended:        {6}
++---------------------------------------------------+
+"""
+TRANSCRIPT_MSG          = """{0} [{1}]: {2}
+"""
 
 def usage():
     print __name__ + " --host=<HOSTNAME/IP OF SOCIALMINER> --user=<ADMIN_USERNAME> --password=<ADMIN_PASSWORD>"
+
 
 def make_search_request(url, user, password):
     print "Making a GET request to the URL", url
@@ -39,6 +54,32 @@ def make_search_request(url, user, password):
         print "Error response:\n", response.text
         sys.exit(1)
     return response.text
+
+
+def compose_transcript_metadata(transcript_node, host, user):
+    return TRANSCRIPT_METADATA.format(host,
+                                      user,
+                                      time.strftime(TIMESTAMP_FORMAT, time.localtime(time.time())),
+                                      transcript_node.find('id').text,
+                                      transcript_node.find('chatInitiator').text,
+                                      time.strftime(TIMESTAMP_FORMAT, time.localtime(float(transcript_node.find('startDate').text)/1000)),
+                                      time.strftime(TIMESTAMP_FORMAT, time.localtime(float(transcript_node.find('endDate').text)/1000)));
+
+
+def extract_chat_messages(transcript_node):
+    chat_messages = ""
+    for chat_message in transcript_node.iter('chat'):
+        chat_messages += TRANSCRIPT_MSG.format(chat_message.find('name').text,
+                                               time.strftime(TIME_FORMAT, time.localtime(float(chat_message.find('time').text)/1000)),
+                                               chat_message.find('msg').text) + "\n"
+
+    return chat_messages
+
+
+def extract_transcript(transcript_node, host, user):
+    transcript_content = compose_transcript_metadata(transcript_node, host, user)
+    transcript_content += "\n" + extract_chat_messages(transcript_node)
+    return transcript_content
 
 
 def main():
@@ -57,17 +98,9 @@ def main():
     root = ElementTree.fromstring(search_response)
 
     for chat_transcript in root.iter('ChatTranscript'):
-        print "+--------------- WEB CHAT TRANSCRIPT ---------------+"
-        print "| Exported from Cisco SocialMiner [", host, "] by '", username, "' at ", \
-            time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        print "|"
-        print "| ID:          ", chat_transcript.find('id').text
-        print "| Customer:    ", chat_transcript.find('chatInitiator').text
-        print "| Started:     ", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(chat_transcript.find('startDate').text)/1000))
-        print "| Ended:       ", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(chat_transcript.find('endDate').text)/1000))
-        print "+---------------------------------------------------+"
+        transcript_text = extract_transcript(chat_transcript, host, username)
+        print transcript_text
 
-    # TODO - parse the response, extract all ChatTranscripts
     # TODO - for each ChatTranscript, create a text file per SC and dump contents
 
 if __name__ == '__main__':
